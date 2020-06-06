4dd172d832c87425a95307d064d738a18e91a131
Matthieu Herchy <matthieu.herchy@enedis-grdf.fr>
2020-06-06 23:30:12 +0200

    rand-token

--- app.js
+++ app.js
@@ -1,16 +1,17 @@
 var express = require('express');
+var randtoken = require('rand-token');
 var app = express();
 
 app.get('/login', function (req, res) {
-    console.log("LOGIN request");
-    console.log(req.body);
-    res.json({token:"0123456789"});
+    console.log("LOGIN requested");
+    const token = randtoken.generate(16);
+    console.log("token : "+token);
+    res.json({token: token});
 });
 
 app.post('/sync', function (req, res) {
-    console.log("SYNC request");
-    console.log(req.body);
-    res.json({feedback:"ok"});
+    console.log("SYNC requested");
+    res.json({feedback: "ok"});
 });
 
 var server = app.listen(8008, function () {
